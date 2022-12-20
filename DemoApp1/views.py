8b3b45490cfeb17b51e8a09106d0ba2f96589ed8
pavankalyanthammisetty2000 <pavankalyanthammisetty2000@gmail.com>
2022-12-20 17:31:15 +0530

    second commit

--- DemoApp1/views.py
+++ DemoApp1/views.py
@@ -6,3 +6,6 @@ def f1(request):
 	return HttpResponse("<h1>Hello from DemoApp1 f1()</h1><hr />");
 def f2(request):
 	return HttpResponse("<h1>Hello from DemoApp1 f2()</h1><hr />");
+
+def f111(request):
+	return HttpResponse("<h1>Hello from DemoApp1 f111()</h1><hr />");
