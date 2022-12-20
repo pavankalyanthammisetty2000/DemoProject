e15de0daf1420b522352a5cced17d470bb7d6b66
pavankalyanthammisetty2000 <120723943+pavankalyanthammisetty2000@users.noreply.github.com>
2022-12-20 17:33:45 +0530

    Update views.py
    third commit

--- DemoApp1/views.py
+++ DemoApp1/views.py
@@ -9,3 +9,7 @@ def f2(request):
 
 def f111(request):
 	return HttpResponse("<h1>Hello from DemoApp1 f111()</h1><hr />");
+
+
+def f222(request):
+	return HttpResponse("<h1>Hello from DemoApp1 f222()</h1><hr />");
